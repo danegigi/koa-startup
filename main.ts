8b7aeb7cb67fdd0ece5357f7765a32f1b49d6ccd
dane <dane.abat@gmail.com>
2023-02-25 22:09:49 +0800

    bun!

--- main.ts
+++ main.ts
@@ -4,11 +4,12 @@ import onerror from 'koa-onerror'
 import bodyParser from 'koa-bodyparser'
 import json from 'koa-json'
 import logger from 'koa-logger'
-import serve from 'koa-static'
-import path from 'path'
+import serve from 'koa-better-serve'
+
+import path from 'node:path'
 
 // routes
-import {users, common} from "./routes"
+import {users, common} from "./routes/index"
 
 import http from 'http'
 
@@ -25,18 +26,17 @@ app.use(bodyParser({
   enableTypes:['json', 'form', 'text']
 }))
 
-const staticPath = path.join(__dirname, 'public')
-console.log("static path", staticPath)
-app.use( serve(staticPath, {
-
-}))
-
 // routes
 router.use("", common.routes(), common.allowedMethods())
 router.use("/users", users.routes(), users.allowedMethods())
 
 app.use(router.routes())
 app.use(router.allowedMethods())
+
+// static files
+const staticPath = path.join(__dirname, 'public')
+app.use( serve(staticPath, '/static'))
+
 
 app.on('error', (err, ctx) => {
   console.error('Server Error', err, ctx)
